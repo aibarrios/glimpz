c0b8d55144046a8483449dbc13d43f3f56f6806e
Alexander Barrios <50901207+aibarrios@users.noreply.github.com>
2022-03-22 18:07:27 +0800

    Editted path at localDataRead.js

--- utils/localDataRead.js
+++ utils/localDataRead.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
 //As soon as the server starts we save the object parsed JSON into tours variable.
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+  fs.readFileSync(
+    path.join(__dirname, `../dev-data/data/tours-simple.json`),
+    'utf-8'
+  )
 );
 
 module.exports = tours;
