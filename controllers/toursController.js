2e7c85280be9d2d6c9c455559ae058e6b9c697d0
Alexander Barrios <50901207+aibarrios@users.noreply.github.com>
2022-03-23 18:49:18 +0800

    Refactoring code, added models and toursModel, start integrating MongoDB

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,10 +1,5 @@
 //App logic for tours router
 
-const fs = require('fs');
-
-//reading our local dev-data as soon as we start our server
-const tours = require('./../utils/localDataRead');
-
 //middleware to check body from POST request, must have both name and price
 exports.checkBody = (req, res, next) => {
   const body = req.body;
@@ -18,44 +13,37 @@ exports.checkBody = (req, res, next) => {
 };
 
 //Param middleware (req,res,next,val) => {next()}, check if the url have specific param
-exports.checkID = (req, res, next, val) => {
-  // req.params is where all variables (example: id) in the url are stored
-  //Check if the ID number is within the range of available number of tours
-  //-1 because it array starts with 0
-  //We've multiplied it by 1 to coerce the id string type to number type
-  if (req.params.id * 1 > tours.length - 1) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// exports.checkID = (req, res, next, val) => {
+// req.params is where all variables (example: id) in the url are stored
+//Check if the ID number is within the range of available number of tours
+//-1 because it array starts with 0
+//We've multiplied it by 1 to coerce the id string type to number type
+//   if (req.params.id * 1 > tours.length - 1) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID',
+//     });
+//   }
+//   next();
+// };
 
 //jsend format res.status(<statusCode>).json({status: 'success/fail', data: {<dataHere>}})
 exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length, //not necessary if array length is only one
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  //Find tour with matching id from tours array
-  const tour = tours.find((tour) => {
-    return tour.id === id;
-  });
-
   res.status(200).json({
     status: 'success',
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
 
@@ -69,49 +57,17 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  //to delete
-  //create copy of tours array
-  //filter array, returns all elements which ID is not the ID to be deleted
-  //write to File returned from filtered array
-  const id = req.params.id * 1;
-  const newTour = [...tours].filter((tour) => {
-    return tour.id !== id;
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(newTour),
-    'utf-8',
-    (error) => {
-      if (error) return error;
-      //204
-      return res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    }
-  );
 };
 
 exports.createTour = (req, res) => {
-  const data = req.body;
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, data); //returns new Object with additional key-value pairs.
-  //We add the new data from newTour object to our existing tours array.
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours), //since our file is in JSON format we will transform our data into JSON format using stringify method
-    'utf-8',
-    (error) => {
-      //this is a guard clause, as soon as we receive an error we wil return the error and close the function call.
-      if (error) return error;
-      //201
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
+  });
 };
---
+++ models/toursModel.js
@@ -0,0 +1,45 @@
+//Business Logic (Data)
+
+//MongoDB - Mongoose Setup
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//We will be using mongoose.js an Object Data Mapper (ODM) for MongoDB-Node.js
+const mongoose = require('mongoose');
+//mongoose.connect('<database_uri>', {options});
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log(`Connected to the database...`))
+  .catch((error) => console.error(error.message));
+
+//Schema (Reference for type|value|validation) -> Model (Blueprint / like Class in JS)
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A name is required to create new tour'],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please specify a price for this tour'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+//Creating new model = collection
+const Tour = new mongoose.model('Tour', tourSchema);
+//Sample document creation
+// const tour = new Tour({
+//   name: 'The Forest Hiker',
+//   price: 497,
+// });
+// tour.save();
+
+module.exports = Tour;
