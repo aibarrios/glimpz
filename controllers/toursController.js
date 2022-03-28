bbfe4976de81ad11d5a6394b04df31d052a4ebbb
Alexander Barrios <50901207+aibarrios@users.noreply.github.com>
2022-03-28 18:13:10 +0800

    modified the tours MVC

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -1,16 +1,18 @@
 //App logic for tours router
 
+const Tour = require('../models/toursModel');
+
 //middleware to check body from POST request, must have both name and price
-exports.checkBody = (req, res, next) => {
-  const body = req.body;
-  if (!(body.name && body.price)) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Bad request, missing some properties.',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   const body = req.body;
+//   if (!(body.name && body.price)) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Bad request, missing some properties.',
+//     });
+//   }
+//   next();
+// };
 
 //Param middleware (req,res,next,val) => {next()}, check if the url have specific param
 // exports.checkID = (req, res, next, val) => {
@@ -28,46 +30,94 @@ exports.checkBody = (req, res, next) => {
 // };
 
 //jsend format res.status(<statusCode>).json({status: 'success/fail', data: {<dataHere>}})
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Successfully updated...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      tour: newTour,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
 };
--- models/toursModel.js
+++ models/toursModel.js
@@ -13,8 +13,6 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   })
   .then(() => console.log(`Connected to the database...`))
   .catch((error) => console.error(error.message));
@@ -25,14 +23,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A name is required to create new tour'],
     unique: true,
+    trim: true,
   },
   price: {
     type: Number,
     required: [true, 'Please specify a price for this tour'],
   },
+  discount: Number,
+  duration: {
+    type: Number,
+    required: [true, 'Please specify the duration of this tour.'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Please specify the difficulty of this tour.'],
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Please give a brief summary for this tour.'],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'Please describe this tour.'],
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image.'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDate: [Date],
   rating: {
     type: Number,
     default: 4.5,
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
 });
 
